fbd76d7204b4b5229b6e65e99e3d24e5bc849665
esaci <elias.saci@gmail.com>
2021-07-05 16:38:46 +0200

    auto

--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 
-set -e
 
-if [ ! -f "wp-config.php" ]; then
-	printf "Seems like wordpress volume is missing something. Let's try to solve the problem!\n"
-	if ! wp core download; then
-		printf "core was installed, but config was missing.\n"
-	fi
-	cp -r /var/www/wordpress/conf/wp-config.php .
-fi
-
+count=0
 while !( mariadb -h mariadb -P 3306 -u wordpress -p wordpress); do
+		if [ $count -gt 60 ]; then
+			printf "Erreur connection\n"
+			exit 1
+		fi
+		count=$((count+1))
 		sleep 1
-		printf " Toujours pas connecte...\n"
 	done
 printf "Connection etablie.\n"
 
@@ -20,12 +16,7 @@ if [ "$(mariadb -h mariadb -P 3306 -u wordpress -p wordpress -e "USE wordpress; 
 	wp core install --url="http://localhost:9000" --title="Inception esaci" --admin_user="esaci"	\
 		--admin_password="pwd" --admin_email="elias@gmail.com" --skip-email
 	wp user create user1 user1@example.com --role=editor --user_pass=pass
-	wp user create user2 user2@example.com --role=author --user_pass=pass
-	wp user create user3 user3@example.com --role=contributor --user_pass=pass
-	wp user create user4 user4@example.com --role=subscriber --user_pass=pass
-	wp post create /tmp/article1.txt --post_author=user1 --post_category="Articles" --post_title="esaci Article 1" --post_status=publish  > /tmp/postid
 	echo "<?php echo 'php-fpm works' ?>" > /tmp/test.php
-	done
 fi
 
 php-fpm7 -F

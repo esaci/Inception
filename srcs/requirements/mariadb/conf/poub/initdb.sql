d3ff761e4893249a53932f6ee047c99ff14a9c12
esaci <elias.saci@gmail.com>
2021-07-07 16:23:37 +0200

    auto

--- srcs/requirements/mariadb/conf/poub/initdb.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'elias'@'%' IDENTIFIED BY 'elias';
-GRANT ALL PRIVILEGES ON *.* TO 'elias'@'%';
-FLUSH PRIVILEGES;
-update mysql.user set plugin = 'mysql_native_password' where user='root';

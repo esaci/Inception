19bc79ea4b19d7dcd9a1e4a09420ce307cfead9e
esaci <elias.saci@gmail.com>
2021-07-09 17:17:36 +0200

    auto

--- srcs/requirements/nginx/conf/start.sh
+++ srcs/requirements/nginx/conf/start.sh
@@ -1,4 +1,7 @@
 sed -i "s/ssl_certificate SSL_CERTIF;/ssl_certificate ${SSL_CERTIF};/g" /etc/nginx/conf.d/default.conf
-sed -i "s/ssl_certificate_key SSL_KEY/ssl_certificate ${SSL_KEY}/g" /etc/nginx/conf.d/default.conf
-sed -i "s/ssl_protocols TLSV/ssl_certificate ${TLSV}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/ssl_certificate_key SSL_KEY/ssl_certificate_key ${SSL_KEY}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/ssl_protocols TLSV/ssl_protocols ${TLSV}/g" /etc/nginx/conf.d/default.conf
 sed -i "s/WP_PORT/${WP_PORT}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/WP_NOM/${WP_NOM}/g" /etc/nginx/conf.d/default.conf
+
+nginx -g 'daemon off;'

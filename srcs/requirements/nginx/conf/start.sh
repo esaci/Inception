3eadeebbe1a80f3dd4c96e4974b022effee21e12
esaci <elias.saci@gmail.com>
2021-07-09 17:21:20 +0200

    auto

--- srcs/requirements/nginx/conf/start.sh
+++ srcs/requirements/nginx/conf/start.sh
@@ -4,4 +4,6 @@ sed -i "s/ssl_protocols TLSV/ssl_protocols ${TLSV}/g" /etc/nginx/conf.d/default.
 sed -i "s/WP_PORT/${WP_PORT}/g" /etc/nginx/conf.d/default.conf
 sed -i "s/WP_NOM/${WP_NOM}/g" /etc/nginx/conf.d/default.conf
 
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=FR/ST=France/L=Paris/O=42/CN=esaci.42.fr" -keyout ${SSL_KEYY} -out ${SSL_CERTIFF}
+
 nginx -g 'daemon off;'

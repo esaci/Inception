ada84d1323d8f8234e1834f6b65834942f56b8ad
esaci <elias.saci@gmail.com>
2021-07-10 17:03:35 +0200

    auto

--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -30,9 +30,6 @@ if [ "$(mariadb -h${MARIADB_HOST} -P${MARIADB_PORT} -u${MARIADB_USER} -p${MARIAD
 		--admin_password="${WP_ADMIN_PWD}" --admin_email="elias@gmail.com" --skip-email
 	wp term create category "Elias Premier Article"
 	wp user create user1 user1@example.com --role=editor --user_pass=pass
-	wp user create user2 user2@example.com --role=author --user_pass=pass
-	wp user create user3 user3@example.com --role=contributor --user_pass=pass
-	wp user create user4 user4@example.com --role=subscriber --user_pass=pass
 	wp post create /tmp/article1.txt --post_author=1 --post_category="Elias Premier Article" --post_title="Merci a tous" --post_status=publish  > /tmp/postid
 fi
 
